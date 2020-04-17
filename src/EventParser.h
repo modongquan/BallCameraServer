19036d7c9d29629135d4010f09988a7d24c41278
modongquan <mfml709394@163.com>
2020-04-17 10:39:14 +0800

    config encode bite rate and fix rtmp bug

---
+++ src/EventParser.h
@@ -0,0 +1,19 @@
+#ifndef EVENTPARSER_H
+#define EVENTPARSER_H
+
+#include <stdint.h>
+
+typedef struct{
+    int32_t left_top_x;
+    int32_t left_top_y;
+    int32_t right_bottom_x;
+    int32_t right_bottom_y;
+}StruDefCoordinate;
+
+int32_t ParseDeviceId(const char *strEvent, char *pDevId);
+int32_t ParseEventType(const char *strEvent, int32_t *pEventType);
+int32_t ParseAlarmType(const char *strEvent, int32_t *pAlarmType);
+int32_t ParseSubAlarmType(const char *strEvent, int32_t *pSubAlarmType);
+int32_t ParseCoordinates(const char *strEvent, StruDefCoordinate *pCoordinates, uint32_t *pNums);
+
+#endif // EVENTPARSER_H
